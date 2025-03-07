69a68a00faa398a95e927df97a8fab40766f5dc0
Kamanashis-Biswas <kamanashis.cse@gmail.com>
2025-03-07 17:16:45 +0600

    modal hight weight increase

--- src/pages/Haircare.jsx
+++ src/pages/Haircare.jsx
@@ -2,7 +2,7 @@ import { useEffect, useState } from "react";
 import { Modal } from "flowbite-react";
 import { Link } from "react-router-dom";
 import { FaRegStar, FaShoppingCart } from "react-icons/fa";
-import haircareCover from "../assets/cover/haircare.jpg"; 
+import haircareCover from "../assets/cover/haircare.jpg";
 import { FaStar } from "react-icons/fa6";
 import { AiOutlineDown, AiOutlineUp } from "react-icons/ai";
 
@@ -90,73 +90,76 @@ const Haircare = () => {
       {/* Modal Section */}
       {selectedProduct && (
         <Modal
-          className="absolute"
+          className="fixed inset-0 flex items-center justify-center z-50"
           show={openModal}
           onClose={() => setOpenModal(false)}
         >
-          {/* Close Button (Right Side) */}
-          <button
-            className="absolute top-4 right-4 w-8 h-8 rounded-full -mt-8 -mr-8 text-white bg-primary flex items-center justify-center hover:bg-primary-dark transition-all duration-300 ease-in-out"
-            onClick={() => setOpenModal(false)}
-          >
-            X
-          </button>
+          <div className="fixed inset-0 bg-black opacity-50"></div>
+          <div className="relative bg-white w-[700px] h-[500px] p-6 rounded-lg shadow-lg z-10">
+            {/* Close Button (Right Side) */}
+            <button
+              className="absolute -top-4 -right-4 w-8 h-8 rounded-full text-white bg-primary flex items-center justify-center hover:bg-primary-dark transition-all duration-300 ease-in-out"
+              onClick={() => setOpenModal(false)}
+            >
+              X
+            </button>
 
-          <div className="flex flex-col md:flex-row items-center gap-4 p-6 font-poppins">
-            <div className="w-full md:w-1/2 p-2">
-              <img src={selectedProduct.image} alt={selectedProduct.name} />
-            </div>
-            <div className="w-full md:w-1/2 p-2">
-              <h2 className="text-gray-800 text-xl md:text-2xl mb-3 font-bold">
-                {selectedProduct.name}
-              </h2>
-              <div className="flex items-center gap-1 mb-3">
-                <FaStar className="text-yellow-400" />
-                <FaStar className="text-yellow-400" />
-                <FaStar className="text-yellow-400" />
-                <FaStar className="text-yellow-400" />
-                <FaRegStar className="text-yellow-400" />
+            <div className="flex flex-col md:flex-row h-full justify-center items-center gap-4 p-4 font-poppins">
+              <div className="w-full h-full md:w-1/2 p-2">
+                <img src={selectedProduct.image} alt={selectedProduct.name} />
               </div>
-              <p className="text-xl md:text-2xl font-bold text-green-600 mb-3">
-                ৳ {selectedProduct.price}
-              </p>
-              <Link
-                to="https://www.facebook.com/wholesaleCosmeticsGallerybd"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                <button className="flex gap-2 justify-center items-center w-full bg-primary text-white px-6 py-2 rounded-lg hover:bg-primary-dark transition-all">
-                  <FaShoppingCart />
-                  BUY NOW
-                </button>
-              </Link>
+              <div className="w-full h-full md:w-1/2 p-2">
+                <h2 className="text-gray-800 text-xl md:text-2xl mb-3 font-bold">
+                  {selectedProduct.name}
+                </h2>
+                <div className="flex items-center gap-1 mb-3">
+                  <FaStar className="text-yellow-400" />
+                  <FaStar className="text-yellow-400" />
+                  <FaStar className="text-yellow-400" />
+                  <FaStar className="text-yellow-400" />
+                  <FaRegStar className="text-yellow-400" />
+                </div>
+                <p className="text-xl md:text-2xl font-bold text-green-600 mb-3">
+                  ৳ {selectedProduct.price}
+                </p>
+                <Link
+                  to="https://www.facebook.com/wholesaleCosmeticsGallerybd"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  <button className="flex gap-2 justify-center items-center w-full bg-primary text-white px-6 py-2 rounded-lg hover:bg-primary-dark transition-all">
+                    <FaShoppingCart />
+                    BUY NOW
+                  </button>
+                </Link>
 
-              <div className="mt-6 relative">
-                <p className="text-lg md:text-xl font-bold">About</p>
-                <div>
-                  <p className="text-sm md:text-base">
-                    {isExpanded
-                      ? selectedProduct.description
-                      : selectedProduct.description.length > 50
-                      ? selectedProduct.description.slice(0, 50) + "..."
-                      : selectedProduct.description}
-                  </p>
-                  {selectedProduct.description.length > 50 && (
-                    <button
-                      onClick={toggleExpand}
-                      className="absolute bottom-2 right-2 cursor-pointer flex items-center"
-                    >
-                      {isExpanded ? (
-                        <>
-                          <AiOutlineUp className="absolute " />
-                        </>
-                      ) : (
-                        <>
-                          <AiOutlineDown className=" absolute " />
-                        </>
-                      )}
-                    </button>
-                  )}
+                <div className="mt-6 relative">
+                  <p className="text-lg md:text-xl font-bold">About</p>
+                  <div>
+                    <p className="text-sm md:text-base">
+                      {isExpanded
+                        ? selectedProduct.description
+                        : selectedProduct.description.length > 50
+                        ? selectedProduct.description.slice(0, 50) + "..."
+                        : selectedProduct.description}
+                    </p>
+                    {selectedProduct.description.length > 50 && (
+                      <button
+                        onClick={toggleExpand}
+                        className="absolute bottom-2 right-2 cursor-pointer flex items-center"
+                      >
+                        {isExpanded ? (
+                          <>
+                            <AiOutlineUp className="absolute " />
+                          </>
+                        ) : (
+                          <>
+                            <AiOutlineDown className=" absolute " />
+                          </>
+                        )}
+                      </button>
+                    )}
+                  </div>
                 </div>
               </div>
             </div>
